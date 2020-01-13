afe8124bd8387bd05b9a858ab2c02e788451dd50
damiecode <damidigi@yahoo.com>
2020-01-13 22:45:27 +0100

    Render books to webpage

--- js/library.js
+++ js/library.js
@@ -10,8 +10,13 @@ function Book(title, author, pages, readAlready) {
 function addBookToLibrary() {
   let title = document.getElementById("book_title").value;
   let author = document.getElementById("book_author").value;
-  let pages = document.getElementById("pages").value;
-  let readAlready = document.getElementById("readAlready").value;
+  let pages = parseInt(document.getElementById("pages").value)
+  let readAlready = document.getElementsByName("readAlready")[0].checked;
+  if(readAlready == true) {
+    readAlready = "Read"
+  } else {
+    readAlready = "Not Read"
+  }
 
   let newBook = new Book(title, author, pages, readAlready);
   myLibrary.push(newBook);
@@ -21,6 +26,6 @@ function addBookToLibrary() {
 function render() {
   document.getElementById("title").innerHTML = myLibrary[0]["title"];
   document.getElementById("author").innerHTML = myLibrary[0]["author"];
-  document.getElementById("pages").innerHTML = myLibrary[0]["pages"]; 
-  document.getElementById("read").innerHTML = myLibrary[0]["read"];
+  document.getElementById("pages").innerHTML = myLibrary[0]["pages"];
+  document.getElementById("read").innerHTML = myLibrary[0]["readAlready"];
 }
