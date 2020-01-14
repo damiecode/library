020b878249f0fb321da4330fd300171dc56e2916
Miguel Prada <mapra99@gmail.com>
2020-01-14 14:57:06 -0500

    fixed linter errors

--- js/library.js
+++ js/library.js
@@ -12,37 +12,21 @@ function Book(title, author, pages, readAlready) {
 
 Book.prototype = {
   constructor: Book, // define the constructor property
-  updateReadStatus: function () {
+  updateReadStatus() {
     if (this.readAlready === 'been read') {
       this.readAlready = 'not been read';
     } else {
       this.readAlready = 'been read';
     }
-  }
+  },
 };
-myLibrary.forEach(book => book.__proto__ = new Book())
-function addBookToLibrary() {
-  let title = document.getElementById("book_title").value;
-  let author = document.getElementById("book_author").value;
-  let pages = parseInt(document.getElementById("pages").value);
-  let readAlready = document.getElementsByName("readAlready")[0].checked;
-  if(readAlready == true) {
-    readAlready = "been read";
-  } else {
-    readAlready = "not been read";
-  }
 
-  let newBook = new Book(title, author, pages, readAlready);
-  myLibrary.push(newBook);
-  updateLocalStorage(myLibrary);
-  render();
-  document.getElementById('id01').style.display='none';
-  document.getElementById('form').reset();
-}
+myLibrary.forEach((book) => { Object.setPrototypeOf(book, Book.prototype); });
 
 function render() {
-  document.getElementById("books-list").innerHTML = '';
-  myLibrary.forEach((book,index) => document.getElementById("books-list").innerHTML += `
+  document.getElementById('books-list').innerHTML = '';
+  myLibrary.forEach((book, index) => {
+    document.getElementById('books-list').innerHTML += `
 
    <div class="col-12 col-md-3 col-lg-4">
       <ul>
@@ -53,24 +37,43 @@ function render() {
         <button onclick="updateReadStatus(this)" data-attributes = ${index} >Update Read Status</button>
         <button onclick="removeBookFromLibrary(this)" data-attributes = ${index} >Delete</button>
       </ul>
-    </div>
-        `
-  )};
-
-  function updateReadStatus(book){
-    var bookToUpdate = book.getAttribute("data-attributes");
-    myLibrary[bookToUpdate].updateReadStatus();
-    updateLocalStorage(myLibrary);
-    render();
-  }
-
-  function removeBookFromLibrary(book) {
-    var bookToDelete = book.getAttribute("data-attributes");
-    myLibrary.splice(bookToDelete, 1);
-    updateLocalStorage(myLibrary);
-    render();
-  }
+    </div>`;
+  });
+}
 
 function updateLocalStorage(array) {
-  window.localStorage.setItem('library', JSON.stringify( array ));
+  window.localStorage.setItem('library', JSON.stringify(array));
+}
+
+function addBookToLibrary() {
+  const title = document.getElementById('book_title').value;
+  const author = document.getElementById('book_author').value;
+  const pages = parseInt(document.getElementById('pages').value, 16);
+  let readAlready = document.getElementsByName('readAlready')[0].checked;
+  if (readAlready === true) {
+    readAlready = 'been read';
+  } else {
+    readAlready = 'not been read';
+  }
+
+  const newBook = new Book(title, author, pages, readAlready);
+  myLibrary.push(newBook);
+  updateLocalStorage(myLibrary);
+  render();
+  document.getElementById('id01').style.display = 'none';
+  document.getElementById('form').reset();
+}
+
+function updateReadStatus(book) {
+  const bookToUpdate = book.getAttribute('data-attributes');
+  myLibrary[bookToUpdate].updateReadStatus();
+  updateLocalStorage(myLibrary);
+  render();
+}
+
+function removeBookFromLibrary(book) {
+  const bookToDelete = book.getAttribute('data-attributes');
+  myLibrary.splice(bookToDelete, 1);
+  updateLocalStorage(myLibrary);
+  render();
 }
